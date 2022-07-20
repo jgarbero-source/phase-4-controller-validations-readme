fbc786d4e9590882eabcaf4ffb6bd5f957528b8e
John Garbero <john.garbero.itb@gmail.com>
2022-07-20 10:58:26 -0500

    yay

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -10,7 +10,11 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    if bird.valid?
+      render json: bird, status: :created
+    else
+      render json: { errors: bird.errors }, status: :unprocessable_entity
+    end
   end
 
   # GET /birds/:id
@@ -24,6 +28,8 @@ class BirdsController < ApplicationController
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
